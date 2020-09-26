9f2452d0d901554864b05302212945ce709ea068
QIDBNET\digtras_devbe1 <stalskaper@outlook.com>
2020-09-26 17:50:15 +0300

    update with task requirements

--- LeetCodeStringToInt/Program.cs
+++ LeetCodeStringToInt/Program.cs
@@ -25,6 +25,7 @@ namespace LeetCodeStringToInt
     public static class Solution
     {
         public static char[] limitValue = new char[]{'2','1','4','7','4','8','3','6','4','8'};
+        public static char[] separators = new char[] { ' ', '.', ',', };
         public static Dictionary<char, int> charToDigit = new Dictionary<char, int>()
         {
             { '0',0 },
@@ -39,19 +40,24 @@ namespace LeetCodeStringToInt
             { '9', 9}
         };
         public static int MyAtoi(string str)
-        {            
+        {       
             int result = 0;
-            List<char> toProcess = str.Trim().Split(' ').FirstOrDefault(a=>a.All(c => Char.IsDigit(c) || c == '-')).Trim().ToList<char>();
+            List<char> toProcess = str.Trim().Split(separators).FirstOrDefault().Trim(separators).ToList<char>();
+            if (toProcess.Count(t => charToDigit.ContainsKey(t)) == 0 || (!toProcess.All(c => Char.IsDigit(c) || c == '-' || c=='+') && !toProcess.Any(t => charToDigit.ContainsKey(t)))) return 0;
             bool sign = toProcess[0] == '-';
-            if (sign)
+            if (sign || toProcess[0] == '+')
             {
+                if(!charToDigit.ContainsKey(toProcess[1])) return 0;
                 toProcess.RemoveAt(0);
             }
+            while (toProcess.Count()>0 && toProcess[0] == '0') toProcess.RemoveAt(0);
+            if (toProcess.Count()<=0) return 0;
             int counter = toProcess.Count();
             if ((toProcess.Count)<10)
             {
                 do
                 {
+                    if (!char.IsNumber(toProcess[counter - 1])) break;
                     result+=(int)Math.Pow(10, toProcess.Count() - counter)*(charToDigit[toProcess[counter - 1]]);
                     counter--;
                 } while (counter >0);
