b1f73efe2b314046e3cd0c748c955a090354333e
QIDBNET\digtras_devbe1 <stalskaper@outlook.com>
2020-09-27 23:22:14 +0300

    success Runtime: 80 ms, faster than 68.86% of C# online submissions for String to Integer (atoi). Memory Usage: 25.9 MB, less than 9.00% of C# online submissions for String to Integer (atoi).

--- LeetCodeStringToInt/Program.cs
+++ LeetCodeStringToInt/Program.cs
@@ -44,36 +44,55 @@ namespace LeetCodeStringToInt
         {
             startPosition = -1; 
             finishPosition = -1;
+            bool nonWhiteSpaceAppeared = false;
+            bool trailingNullsAppeared = false;
             int result = 0;
             int counter = 0;
             sign sign = sign.unassigned;
             while (counter < str.Length)
             {
-                if (signChars.Contains(str[counter]))
+                if (str[counter] == ' ')
                 {
-                    if (sign == sign.unassigned)
-                    { 
-                        sign = (str[counter] == '+') ? sign.positive : sign.negative; 
+                    if (nonWhiteSpaceAppeared)
+                    {
+                        finishPosition = counter - 1;
+                        break;
                     }
-                    else return 0;
+                    counter++;
                 }
-
-                if (startPosition == -1)
+                else if (signChars.Contains(str[counter]))
                 {
-                    if (str[counter] == '0') { counter++; continue; }
-                    if (charToDigit.ContainsKey(str[counter]))
+                    if (startPosition > -1)
+                    {
+                        finishPosition = counter - 1;
+                        break;
+                    }
+                    else if (trailingNullsAppeared) return 0;                    
+                    nonWhiteSpaceAppeared = true;
+                    if (sign != sign.unassigned) return 0;
+                    else sign = str[counter] == '+' ? sign.positive : sign.negative;
+                    counter++;
+                    continue;
+                }
+                else if (charToDigit.ContainsKey(str[counter]))
+                {
+                    nonWhiteSpaceAppeared = true;
+                    if (str[counter] != '0' && startPosition == -1)
                     {
                         startPosition = counter;
-                        if (sign == sign.unassigned) sign = sign.positive;
                     }
+                    else
+                    {
+                        trailingNullsAppeared = true;
+                    }
+                    counter++;
+                    continue;
                 }
                 else
-                if (!charToDigit.ContainsKey(str[counter]))
                 {
-                    finishPosition = counter - 1;
+                    if (startPosition > -1) finishPosition = counter - 1;
                     break;
-                }
-                counter++;
+                }              
             }
             if (startPosition == -1) return 0;
             if (finishPosition == -1) finishPosition = str.Length - 1;
@@ -87,9 +106,12 @@ namespace LeetCodeStringToInt
                     result += myPow(10, parsebleArray.Count() - counter) * (charToDigit[parsebleArray[counter - 1]]);
                     counter--;
                 } while (counter > 0);
-                return sign == sign.positive ? result : (-1) * result;
+                return sign == sign.negative ? (-1) * result : result;
             }
-            else return sign== sign.positive ? parseMax(parsebleArray)*(-1) :  parseMax(parsebleArray);
+            else
+            {
+                return parseMax(parsebleArray, sign != sign.negative);
+            }
         }
 
         public static Int32 myPow(int baseValue, int exponentValue)
@@ -105,15 +127,30 @@ namespace LeetCodeStringToInt
             return baseValue * myPow(baseValue, exponentValue - 1);
         }
 
-        public static int parseMax(char[] parsedString)
+        public static int parseMax(char[] parsedString, bool positive = true)
         {
-            if (parsedString.Length > 10) return Int32.MinValue;
+            if (parsedString.Length > 10) return positive ? Int32.MaxValue : Int32.MinValue;
+            bool hasGap = false;
             Int32 result = 0;
-            for (int i = 0; i < parsedString.Length; i++)
+            for (int i = 0; i < parsedString.Length - 1; i++)
             {
-                if (charToDigit[parsedString[i]] > charToDigit[limitValue[i]]) return Int32.MaxValue;
+                if (charToDigit[parsedString[i]] < charToDigit[limitValue[i]]) hasGap = true;
+                if (charToDigit[parsedString[i]] > charToDigit[limitValue[i]] && !hasGap) return positive ? Int32.MaxValue : Int32.MinValue;
                 else
-                    result -= myPow(10, parsedString.Count() - i - 1) * (charToDigit[parsedString[i]]);
+                    result += myPow(10, parsedString.Count() - i - 1) * (charToDigit[parsedString[i]]);
+            }
+            if (positive) 
+            {
+                if (!hasGap && (charToDigit[parsedString.Last()] > charToDigit[limitValue.Last()] - 1)) return Int32.MaxValue;
+                else
+                    result += (charToDigit[parsedString.Last()]);
+            }
+            else 
+            {
+                result = (-1) * result;
+                if (!hasGap && (charToDigit[parsedString.Last()] > charToDigit[limitValue.Last()])) return Int32.MinValue;
+                else
+                    result -= (charToDigit[parsedString.Last()]);
             }
             return result;
         }
@@ -121,8 +158,8 @@ namespace LeetCodeStringToInt
         public enum sign
         {
             positive,
-            negative,
-            unassigned
+            unassigned,
+            negative
         }
     }
 }
