e080f08ec053bf2749b23ca52eb42cda7db20e94
QIDBNET\digtras_devbe1 <stalskaper@outlook.com>
2020-09-27 14:34:57 +0300

    fixes for preceding letters

--- LeetCodeStringToInt/Program.cs
+++ LeetCodeStringToInt/Program.cs
@@ -24,46 +24,79 @@ namespace LeetCodeStringToInt
 
     public static class Solution
     {
+        public static int startPosition, finishPosition;
         public static char[] limitValue = new char[]{'2','1','4','7','4','8','3','6','4','8'};
         public static char[] separators = new char[] { ' ', '.', ',', };
         public static Dictionary<char, int> charToDigit = new Dictionary<char, int>()
         {
-            { '0',0 },
-            {'1',1 },
-            { '2',2},
-            { '3',3},
-            { '4',4},
-            { '5',5},
-            { '6', 6},
-            { '7', 7},
-            { '8', 8},
-            { '9', 9}
+            {'0', 0},
+            {'1', 1},
+            {'2', 2},
+            {'3', 3},
+            {'4', 4},
+            {'5', 5},
+            {'6', 6},
+            {'7', 7},
+            {'8', 8},
+            {'9', 9}
         };
         public static int MyAtoi(string str)
-        {       
+        {
+            startPosition = -1; 
+            finishPosition = -1;
             int result = 0;
-            List<char> toProcess = str.Trim().Split(separators).FirstOrDefault().Trim(separators).ToList<char>();
-            if (toProcess.Count(t => charToDigit.ContainsKey(t)) == 0 || (!toProcess.All(c => Char.IsDigit(c) || c == '-' || c=='+') && !toProcess.Any(t => charToDigit.ContainsKey(t)))) return 0;
-            bool sign = toProcess[0] == '-';
-            if (sign || toProcess[0] == '+')
+            int counter = 0;
+            bool sign = true;
+            while (counter < str.Length)
             {
-                if(!charToDigit.ContainsKey(toProcess[1])) return 0;
-                toProcess.RemoveAt(0);
+                if (char.IsLetter(str[counter])) return 0;
+                if (startPosition == -1)
+                {
+                    if (charToDigit.ContainsKey(str[counter])) 
+                    {
+                        startPosition = counter;
+                        if (counter > 0)
+                        {
+                            sign = str[counter - 1] != '-';
+                        }
+                    }
+                }
+                else 
+                    if (!charToDigit.ContainsKey(str[counter]))
+                        { 
+                            finishPosition = counter - 1;
+                        break;
+                    }
+                counter++;
             }
-            while (toProcess.Count()>0 && toProcess[0] == '0') toProcess.RemoveAt(0);
-            if (toProcess.Count()<=0) return 0;
-            int counter = toProcess.Count();
-            if ((toProcess.Count)<10)
+            if (startPosition == -1) return 0;
+            if (finishPosition == -1) finishPosition = str.Length - 1;
+            char[] parsebleArray = str.Substring(startPosition, finishPosition - startPosition + 1).ToCharArray();
+            counter = parsebleArray.Length;
+            if ((parsebleArray.Length) < 10)
             {
                 do
                 {
-                    if (!char.IsNumber(toProcess[counter - 1])) break;
-                    result+=(int)Math.Pow(10, toProcess.Count() - counter)*(charToDigit[toProcess[counter - 1]]);
+                    if (!char.IsNumber(parsebleArray[counter - 1])) break;
+                    result += myPow(10, parsebleArray.Count() - counter) * (charToDigit[parsebleArray[counter - 1]]);
                     counter--;
-                } while (counter >0);
-                return sign ? (-1)*result : result; 
+                } while (counter > 0);
+                return sign ? result : (-1) * result;
             }
             return sign ? Int32.MinValue : Int32.MaxValue;
+        }
+
+        public static Int32 myPow(int baseValue, int exponentValue)
+        {
+            if (exponentValue == 0)
+            {
+                return 1;
+            }
+            if (exponentValue == 1)
+            {
+                return baseValue;
+            }
+            return baseValue * myPow(baseValue, exponentValue - 1);
         }
 
     }
