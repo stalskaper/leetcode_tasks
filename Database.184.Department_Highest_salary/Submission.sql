5f00b9d889be9a0d1c591e125fd4f922e3d549db
Pavlo Dragobetskyi <stalskaper@outlook.com>
2020-12-07 13:43:06 +0200

    database-184-medium

---
+++ Database.184.Department_Highest_salary/Submission.sql
@@ -0,0 +1,6 @@
+﻿Select depN.Name as Department, emp.[Name] as Employee, emp.Salary as Salary from Employee as emp inner join
+(select Department.Id, Max(Salary) as maxs from Employee 
+inner join Department on Employee.DepartmentId = Department.Id
+group by Department.Id) sals on emp.DepartmentId = sals.Id
+inner join Department depN on depN.Id = emp.DepartmentId
+where emp.Salary >= sals.maxs
